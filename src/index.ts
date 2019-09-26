58f83addadfa5d89054f77d20124f25bec30b767
X1 Carbon <yjl9903@vip.qq.com>
2019-09-26 21:29:59 +0800

    update: package

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+#!/usr/bin/env node
+
 import fs from 'fs';
 import NFA from './nfa';
 import DFA from './dfa';
-
-console.log('Start changing NFA into DFA...\n');
 
 if (process.argv.length <= 2) {
   console.log('need param <file>');
   process.exit(0);
 }
+
+console.log('\nStart changing NFA into DFA...\n');
 
 const input = fs.readFileSync(process.argv[2], 'utf8').split('\r\n');
 
